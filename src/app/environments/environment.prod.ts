1b304a10d1eb12c725dc3938880dbdff403b6394
JIBANJYOTIMOHANTY <jibanjyotimohanty315@gmail.com>
2026-06-23 11:22:10 +0530

    feat: configure production environment and add SPA redirect rules for deployment

---
+++ src/app/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+        production: true,
+        apiUrl: 'https://video-streaming-gateway.onrender.com/api/v1' // Replace this placeholder with your actual Render API Gateway web service URL
+};
